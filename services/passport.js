2fb7525c68a61174d15ccb96c9d5b32256fdaa14
executable16 <atulmalakar15@gmail.com>
2021-02-17 17:14:56 +0530

    Promises syntax updated to async await

--- services/passport.js
+++ services/passport.js
@@ -29,18 +29,15 @@ passport.use(new GoogleStrategy({
     callbackURL : '/auth/google/callback',
     proxy : true,
     
-}, (accessToken,refreshToken, profile, done) => {
+}, async (accessToken,refreshToken, profile, done) => {
     
-    User.findOne({googleID : profile.id})
-        .then((existingUser) => {
-            if(existingUser){
-                done(null,existingUser)
-            }
-            else{
-                new User({googleID : profile.id})
-                    .save()
-                    .then((user) => done(null,user))
-                }  
-            })
+        const existingUser = await User.findOne({googleID : profile.id})
+        if(existingUser){
+            done(null,existingUser)
+        }
+        else{
+            const user = await new User({googleID : profile.id}).save()
+            done(null,user)
+        }  
     })
 );
